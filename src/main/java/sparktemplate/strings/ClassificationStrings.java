897367969d32a5f988425e39d2267974d593a051
enexer <as94lol@gmail.com>
2018-08-08 23:34:34 +0200

    Add column names to static fields in strings package

---
+++ src/main/java/sparktemplate/strings/ClassificationStrings.java
@@ -0,0 +1,13 @@
+package sparktemplate.strings;
+
+/**
+ * Created by as on 08.08.2018.
+ */
+public class ClassificationStrings {
+    public static String featuresCol = "features";
+    public static String labelCol = "label";
+    public static String indexedLabelCol = "indexedLabel";
+    public static String indexedFeaturesCol = "indexedFeatures";
+    public static String predictionCol = "prediction";
+    public static String predictedLabelCol ="predictedLabel";
+}
